8056833ce701bd57f68065e813a43620564bad48
ThuanVan Nguyen <nguyenthuankma@gmail.com>
2024-06-07 03:25:46 +0700

    add code feature

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// @ts-check
+
+/**
+ * Pet Name
+ * @type {string}
+ */
+const petName = "Ruby";
+
+
+/**
+ * number
+ * @type {number}
+ */
+const number = 100;
+
+/**
+ * My Array
+ * @type {Array<number>}
+ */
+const myArray = [10, 132.12, 100];
+// const myArray = [10, 132.12, 100, "Hi", true]
+
+/**
+ * Pet object
+ * @type {{id: number, name: string, age: number|string}}
+ */
+const pet = {
+  id: 1,
+  name: "Coco",
+  age: "2",
+};
